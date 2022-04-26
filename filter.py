3b98bc0b2d9769de737228c7c0f6591ceea81615
jorchamo <jorge.chamorro@t-systems.com>
2022-04-26 14:16:41 +0200

    Remove header sorting

--- filter.py
+++ filter.py
@@ -1,11 +1,9 @@
 import json
-
-import ijson
 import click
 import nesteddictionary
 
 from io import TextIOWrapper
-from typing import List, Generator
+from typing import List
 from twarc import ensure_flattened
 
 DEFAULT_FIELDS: List[str] = [
@@ -57,21 +55,20 @@ def generate_nested_keys(tweet, fields):
     return header_keys
 
 
-def load_json_file(infile) -> Generator:
-    json_file = ijson.items(infile, '', multiple_values=True)
-    generator: Generator = (o for o in json_file)
-    return generator
-
-
 @click.command()
-@click.option('-f', '--fields', required=False, default=DEFAULT_FIELDS, multiple=True)
+@click.option('-f', '--fields', required=False, default=None, multiple=False, type=click.STRING)
 @click.option('-e', '--extension', required=False, default='json', type=click.STRING)
 @click.argument('infile', type=click.File('r'), default='-')
 @click.argument('outfile', type=click.File('w'), default='-')
 def twarc_filter(infile: TextIOWrapper,
                  outfile: TextIOWrapper,
-                 fields: List[str],
+                 fields: str,
                  extension: str):
+
+    if fields is None:
+        fields = DEFAULT_FIELDS
+    else:
+        fields = fields.split(',')
 
     csv_headers = set()
     for line in infile:
@@ -85,22 +82,21 @@ def twarc_filter(infile: TextIOWrapper,
 
     if extension == 'csv':
         csv_headers_list = list(csv_headers)
-        csv_headers_list.sort()
         click.echo(','.join(csv_headers_list), file=outfile)
 
         infile.seek(0, 0)
-        tweet_generator: Generator = load_json_file(infile)
-        for tweet in tweet_generator:
-            filtered_tweet = filter_tweet(tweet, fields)
-            nested_tweet = nesteddictionary.NestedDict(filtered_tweet)
-            output_line = []
-            for header in csv_headers_list:
-                try:
-                    value = nested_tweet.get(header)
-                except:
-                    value = ''
-                output_line.append(value)
-            click.echo(','.join(output_line), file=outfile)
+        for line in infile:
+            for tweet in ensure_flattened(json.loads(line)):
+                filtered_tweet = filter_tweet(tweet, fields)
+                nested_tweet = nesteddictionary.NestedDict(filtered_tweet)
+                output_line = []
+                for header in csv_headers_list:
+                    try:
+                        value = nested_tweet.get(header)
+                    except:
+                        value = ''
+                    output_line.append(value)
+                click.echo(','.join(output_line), file=outfile)
 
 
 if __name__ == '__main__':
